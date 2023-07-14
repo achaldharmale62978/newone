23e5bd2f89164b87369d52100feb234cdb24eb69
achaldharmale62978 <achaldharmale62978@gmail.com>
2023-07-14 13:20:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Footer from "./component/Footer/Footer";
+import Header from './component/Header/Header'
+function App() {
+  return (
+    <div className="App">
+     <h2>Hello Dear....</h2>
+     <Footer/>
+     <Header/>
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import "./Footer.css"
+function footer(){
+    return(
+        <>
+        <h3> I am Footer Component....</h3>
+        <p>its jesoon!!!</p>
+        </>
+    );
+}
+export default footer;
